b02bc3ac7993398f7e7a9c81b28a53222ac7aab1
andela-hms <humphrey.musonye@andela.com>
2017-10-18 15:23:58 +0300

    create weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function(state=null, action){
-    
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action){
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // non es6 
+            // return state.concat([action.payload.data]);
+            return [ action.payload.data, ...state ]; // [ city, city, city ]
+    }
     return state;
 }
