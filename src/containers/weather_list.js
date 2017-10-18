41966bdaca82bf7039a6122b80e5cd2f1e47e098
andela-hms <humphrey.musonye@andela.com>
2017-10-18 17:10:37 +0300

    add labels to table heads

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,7 +5,7 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273 );
         const pressure = cityData.list.map(weather => weather.main.pressure);
         const humidity = cityData.list.map(weather => weather.main.humidity);
         
@@ -14,9 +14,9 @@ class WeatherList extends Component {
 
                 <td>{ name }</td>
 
-                <td><Chart data={temps} color="red"/></td>
-                <td><Chart data={pressure} color="green"/></td>
-                <td><Chart data={humidity} color="blue"/></td>
+                <td><Chart data={temps} color="red" units="C" /></td>
+                <td><Chart data={pressure} color="green" units="hPa" /></td>
+                <td><Chart data={humidity} color="blue" units="%" /></td>
             </tr>
         );
     }
@@ -27,9 +27,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
